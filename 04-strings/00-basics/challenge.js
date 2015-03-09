489ada174c76ed422dbc48aa1b0e8d76e319757d
Kateryna <zbsht15@gmail.com>
2015-03-09 17:39:22 +0000

    Solved 5 exercises

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,21 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+	return 'Hello ' + firstName + ',' + '\n' + '\n' +  message + '\n' + '\n' +  'Sincerely,' + '\n' + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+	return bigString.slice(startA, endA).concat(bigString.slice(startB, endB));
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+	return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+	return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+	var stepOne = text.substring(text.indexOf(searchString), text.lastIndexOf(searchString));
+	var stepTwo = stepOne.substring(searchString.length);
+	return stepTwo;
 };
