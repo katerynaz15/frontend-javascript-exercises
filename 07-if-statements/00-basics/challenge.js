9b4279e6a1534ea06827ab7dcbc44864841dfb19
Kateryna <zbsht15@gmail.com>
2015-03-09 23:42:25 +0000

    All is done!

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,32 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(favNum, guessNum) {
+	if (guessNum>favNum) {
+		return "Too high";
+	} else if (guessNum<favNum) {
+		return "Too low";
+	} else {
+		return "You got it!";
+	}
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function (numA, numB, numC, numD) {
+	if ((numA===3 || numA===5 || numA===7) && numB===2 && (numC>=5 && numC<=100) && (numD<9 || numD>20)) {
+		return "correct";
+	} else {
+		return "incorrect";
+	}
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+	if (item=="MacBook Air" && money>=999) {
+		return true;
+	} else if (item=="MacBook Pro" && money>=1299) {
+		return true;
+	} else if (item=="Mac Pro" && money>=2499) {
+		return true
+	} else if (item=="Apple Sticker" && money>=1) {
+		return true;
+	} else {
+		return false;
+	}
+
+};
