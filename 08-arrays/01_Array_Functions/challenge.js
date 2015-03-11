4ae5152dae4cee1c1317ec97949c99898dc53645
Kateryna <zbsht15@gmail.com>
2015-03-11 22:24:52 +0000

    Problems solved

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,23 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(arr) {
+	var reversed = arr.reverse();
+	var plusOne = reversed.unshift(1);
+	//A simple diagram...
+	//unshift -> array <- push
+    //shift   <- array -> pop
+		return reversed;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(arr) {
+	var newString = arr.join('+');
+//var a = ['Wind', 'Rain', 'Fire'];z
+//var myVar1 = a.join();      -> assigns 'Wind,Rain,Fire' to myVar1
+//var myVar2 = a.join(', ');  -> assigns 'Wind, Rain, Fire' to myVar2
+//var myVar3 = a.join(' + '); -> assigns 'Wind + Rain + Fire' to myVar3
+//var myVar4 = a.join('');    -> assigns 'WindRainFire' to myVar4
+		return newString;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(arr) {
+	var newNum = arr.unshift(1);
+		return newNum;
+};
