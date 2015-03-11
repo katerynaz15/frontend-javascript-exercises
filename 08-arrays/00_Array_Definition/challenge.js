83e1bd25c635b36ada2cda7f899a5f60e3c5b768
Kateryna <zbsht15@gmail.com>
2015-03-11 06:14:07 +0000

    00-array def. is done

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,12 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a1, a2, a3, a4) {
+	var a = [a1, a2, a3, a4];
+		return a;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function (arr) {
+	var newNum = [arr.indexOf.length, arr.length];
+	var newString = [arr[0], arr[arr.length-1]]; 
+	//arr.length contains the size of the array, to get
+	// the last element of the array, check arr[arr.length-1]
+		return newNum, newString;
+};
