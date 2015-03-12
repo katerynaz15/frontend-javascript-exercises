1fdd5b373458e44c61942f51a97f9bbd70e89457
Kateryna <zbsht15@gmail.com>
2015-03-12 00:01:20 +0000

    Both functions are great :)

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,15 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(addToTheEnd, arr) {
+	if (addToTheEnd==arr) {
+		return arr;
+	} else {
+		var list = arr;
+		var newAddition = list.push(addToTheEnd);
+		return list;
+	}
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(arr) {
+	var sort = arr.sort();
+	var reverse = sort.reverse();
+		return sort;
+};
