f23488bbf9649dfac0b8836e5f6690329158b05b
Kateryna <zbsht15@gmail.com>
2015-03-16 16:42:46 +0000

    All 4 tasks are solved

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,40 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(stringOne, stringTwo, arr) {
+	var myClass = new Object();
+		myClass.title = stringOne;
+		myClass.duration = stringTwo;
+		myClass.students = arr;
+			return myClass;
+//var myClass = {
+//title: stringOne,
+//duration: stringTwo,
+//students: arr }
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(obj, newProp, newValue) {
+	if (!obj.hasOwnProperty(newProp)) {
+		obj[newProp] = newValue;
+		return obj;
+	} else {
+		return obj;
+	}
+};
 
-module.exports.formLetter = undefined;
-
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(obj) {
+	return 'Hello ' + obj.recipient + ',' + 
+			'\n' + '\n' +  obj.msg + '\n' + '\n' +  'Sincerely,' 
+			+ '\n' + obj.sender;
+	};
+	//var numbers = {
+    //ten: 10}
+	//numbers.ten.toString();
+	//'10'
+			
+module.exports.canIGet = function(item, money) {
+		var apple = {
+		'MacBook Air': 999,
+		'MacBook Pro': 1299,
+		'Mac Pro': 2499,
+		'Apple Sticker': 1
+	}
+	return apple[item] <= money ? true : false;
+}
