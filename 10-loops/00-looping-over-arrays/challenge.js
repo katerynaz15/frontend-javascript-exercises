cd40ef46380410b1edf1518c0e177e6ef77bcd05
Kateryna <zbsht15@gmail.com>
2015-03-16 19:49:14 +0000

    sumNumbers, splitAndLowerCaseString, addIndex are done

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr) {
+	var sum = 0;
+	for (var i = 0; i < arr.length; i++) { 
+		sum += arr[i];
+	} 
+	return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+	var lowerCase = inputString.toLowerCase();
+	var splitString = lowerCase.split(",");
+		return splitString;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function (arr) {
+	var newArray = [];
+	for (var i=0; i<arr.length; i++) {
+         newArray.push(i + " is " + arr[i]);
+    } return newArray;
+};
