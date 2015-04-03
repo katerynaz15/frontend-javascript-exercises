9f0b4e29a67cfc2944f3252423be0b1fd6a9b000
Kateryna <zbsht15@gmail.com>
2015-04-03 00:26:56 +0000

    03 is done

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,20 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+	while(conditionalFn()) {
+		actionFn();
+	}
+};
 
-module.exports.sumNumbers = undefined;
 
+module.exports.sumNumbers = function(arr) {
+	  var total = 0;
+	  var i = 0;
+	  	while (i<arr.length) {
+	  		total += arr[i];
+	  		i++;
+	  	}
+
+   //      for (i=0; i<arr.length; i++) {
+   //          total += arr[i];
+   //      }
+             return total;
+};
