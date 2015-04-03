dc103b3275a4d592590a24e5a040e770bc4e766d
Kateryna <zbsht15@gmail.com>
2015-04-03 00:24:48 +0000

    02 is done

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,25 @@
-module.exports.copy = undefined;
+module.exports.copy = function(obj) {
+	var newObject = {};
+	for (var prop in obj) {
+		newObject[prop] = obj[prop];
+	}
+	return newObject;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+	for (var prop in src) {
+		dest[prop] = src[prop];
+	}
+	return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(obj, arr) {
+	for(var i=0;i<arr.length;i++) {
+              //console.log(obj[arr[i]])  
+		    if (typeof(obj[arr[i]]) == "undefined") {
+			    return false;
+		
+	}
+	} return true;
+}; 
+
